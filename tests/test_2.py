b6335c4e886c5894656ffc171a97a44ec360e665
izaal10 <99273053+izaal10@users.noreply.github.com>
2024-02-06 08:37:42 +0700

    main

--- tests/test_2.py
+++ tests/test_2.py
@@ -2,16 +2,15 @@ import unittest
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 
-class LoginInvalidTestCase(unittest.TestCase):
+class LoginFalseCredentialsTestCase(unittest.TestCase):
 
     @classmethod
     def setUpClass(cls):
-        # Set up headless Firefox
-        options = webdriver.FirefoxOptions()
-        options.add_argument('--headless')
-        cls.browser = webdriver.Firefox(options=options)
+        option = webdriver.FirefoxOptions()
+        option.add_argument('--headless')
+        cls.browser = webdriver.Firefox(options=option)
 
-    def test_invalid_login(self):
+    def test_1_login_false_credentials(self):
         login_url = 'http://localhost/login.php'
         self.browser.get(login_url)
 
@@ -19,7 +18,7 @@ class LoginInvalidTestCase(unittest.TestCase):
         self.browser.find_element(By.ID, 'inputPassword').send_keys('admin')
         self.browser.find_element(By.TAG_NAME, 'button').click()
 
-    def test_error_message(self):
+    def test_2_login_page(self):           
         expected_result = "Wrong usename or password"
         
         actual_result = self.browser.find_element(By.CLASS_NAME, 'checkbox').text
@@ -27,9 +26,7 @@ class LoginInvalidTestCase(unittest.TestCase):
 
     @classmethod
     def tearDownClass(cls):
-        # Close browser 
         cls.browser.quit()
 
 if __name__ == '__main__':
-    # Run the tests
     unittest.main(verbosity=2, warnings='ignore')
