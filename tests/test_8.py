2ed8a10d0f6c1e72cf83581528f0db3a44fb0db3
izaal10 <99273053+izaal10@users.noreply.github.com>
2024-02-06 09:47:39 +0700

    MAIN

--- tests/test_8.py
+++ tests/test_8.py
@@ -2,17 +2,14 @@ import unittest, os
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 
-class XSSDetectionTestCase(unittest.TestCase):
+class XSSTestCase(unittest.TestCase):
 
     @classmethod
     def setUpClass(cls):
         option = webdriver.FirefoxOptions()
         option.add_argument('--headless')
         cls.browser = webdriver.Firefox(options=option)
-        try:
-            cls.url = os.environ['URL']
-        except:
-            cls.url = "http://localhost"
+        cls.url = "http://localhost"
             
     def test(self):
         self.login_correct_credentials()
@@ -31,14 +28,14 @@ class XSSDetectionTestCase(unittest.TestCase):
         self.browser.get(xss_url)
 
         input_field = self.browser.find_element(By.NAME, 'thing')
-        input_value = '<script>alert("XSS Attack!");</script>'
+        input_value = '<script>alert("Bad Stuff Here");</script>'
         input_field.send_keys(input_value)
 
         submit_button = self.browser.find_element(By.NAME, 'submit')
         submit_button.click()
 
         alert = self.browser.switch_to.alert
-        self.assertEqual('XSS Attack!', alert.text)
+        self.assertEqual('Bad Stuff Here', alert.text)
         alert.accept()
 
     @classmethod
